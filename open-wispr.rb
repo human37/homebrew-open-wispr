0e9cea9c035019a92c8661e80f91111257a32262
Ammon Taylor <ammonx9@gmail.com>
2026-03-17 21:14:37 -0600

    Bump to v0.27.0

--- open-wispr.rb
+++ open-wispr.rb
@@ -1,7 +1,7 @@
 class OpenWispr < Formula
   desc "Push-to-talk voice dictation for macOS using Whisper"
   homepage "https://github.com/human37/open-wispr"
-  url "https://github.com/human37/open-wispr.git", tag: "v0.26.0"
+  url "https://github.com/human37/open-wispr.git", tag: "v0.27.0"
   license "MIT"
 
   bottle do
